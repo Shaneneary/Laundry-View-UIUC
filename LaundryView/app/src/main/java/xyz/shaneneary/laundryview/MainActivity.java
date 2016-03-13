634bc414fcfbe3e0dc0d4f6d5767d5e4296d44b0
U-ENTELEXEIA\TheTrueSaiyan <shanennn@gmail.com>
2016-03-13 15:22:24 -0500

    Prettified

--- LaundryView/app/src/main/java/xyz/shaneneary/laundryview/MainActivity.java
+++ LaundryView/app/src/main/java/xyz/shaneneary/laundryview/MainActivity.java
@@ -5,73 +5,104 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnGetData;
-    final String URL = "https://www.laundryalert.com/cgi-bin/urba7723/LMPage?Login=True";
-    String content;
+	Button btnGetData;
+	final String URL = "https://www.laundryalert.com/cgi-bin/urba7723/LMPage?Login=True";
 
-    ProgressDialog mProgressDialog;
+	ArrayList<String> dorms;
+	ArrayAdapter<String> dormsAdapter;
+	ListView dormsList;
+	Elements tableNewData;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        btnGetData = (Button) findViewById(R.id.btnContent);
-        btnGetData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new GetContent().execute();
-            }
-        });
+	ProgressDialog mProgressDialog;
 
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    private class GetContent extends AsyncTask<Void, Void, Void> {
+		btnGetData = (Button) findViewById(R.id.btnContent);
+		btnGetData.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				new GetContent().execute();
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mProgressDialog = new ProgressDialog(MainActivity.this);
-            mProgressDialog.setTitle("Getting Content");
-            mProgressDialog.setMessage("Loading...");
-            mProgressDialog.setIndeterminate(false);
-            mProgressDialog.show();
-        }
 
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                Document doc = Jsoup.connect(URL).get();
-                String rawData = doc.body().text();
-                content = trimData( rawData );
+			}
+		});
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
+		dormsList = (ListView) findViewById(R.id.myListView);
+		dorms = new ArrayList<String>();
+		dormsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, dorms);
 
-        @Override
-        protected void onPostExecute(Void result) {
-            TextView contentTextView = (TextView) findViewById(R.id.myTextView);
-            contentTextView.setText(content);
-            mProgressDialog.dismiss();
-        }
-    }
+		dormsList.setAdapter(dormsAdapter);
 
-    public String trimData(String data) {
-        int start = data.indexOf("Allen");
-        int end = data.indexOf("On site operation");
-        return data.substring(start, end);
-    }
+		setupListViewListener();
+	}
+
+	private void setupListViewListener() {
+		dormsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				dorms.remove(position);
+				dormsAdapter.notifyDataSetChanged();
+			}
+		});
+	}
+
+	private class GetContent extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			mProgressDialog = new ProgressDialog(MainActivity.this);
+			mProgressDialog.setTitle("Getting Content");
+			mProgressDialog.setMessage("Loading...");
+			mProgressDialog.setIndeterminate(false);
+			mProgressDialog.show();
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				Document doc = Jsoup.connect(URL).get();
+				Element table = doc.getElementById("tableb");
+				tableNewData = table.getElementsByTag("tr");
+				tableNewData.remove(0);
+				tableNewData.remove(0);
+				tableNewData.remove(30);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			if( ! tableNewData.isEmpty() ) {
+				for (Element row : tableNewData) {
+					dorms.add(row.text());
+				}
+			}
+			dormsAdapter.notifyDataSetChanged();
+			mProgressDialog.dismiss();
+		}
+	}
 }
